12e1b1bde2db57c0be2437b5f9a7689695d82d65
FabGiese <fabgiese@gmail.com>
2023-02-18 18:16:26 -0300

    Adicionado end-point de listagem de medicos.

--- src/main/java/med/voll/api/paciente/PacienteController.java
+++ src/main/java/med/voll/api/paciente/PacienteController.java
@@ -2,6 +2,7 @@ package med.voll.api.paciente;
 
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +15,7 @@ public class PacienteController {
     @Autowired private PacienteRepository repository;
 
     @PostMapping
+    @Transactional
     public void cadastrar(@RequestBody @Valid DadosCadastroPaciente json) {
         repository.save(new Paciente(json));
     }
