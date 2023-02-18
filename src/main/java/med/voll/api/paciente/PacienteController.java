03a5580e97da46e7740d7aa218b39500eb4acbf0
FabGiese <fabgiese@gmail.com>
2023-02-18 17:05:59 -0300

    Adicionado dominio de Paciente e end-point de cadastro de pacientes.

---
+++ src/main/java/med/voll/api/paciente/PacienteController.java
@@ -0,0 +1,20 @@
+package med.voll.api.paciente;
+
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("pacientes")
+public class PacienteController {
+
+    @Autowired private PacienteRepository repository;
+
+    @PostMapping
+    public void cadastrar(@RequestBody @Valid DadosCadastroPaciente json) {
+        repository.save(new Paciente(json));
+    }
+}
