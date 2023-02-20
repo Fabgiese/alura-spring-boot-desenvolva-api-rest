8c1931338a0fc6631bbe9c22d21437f3e5619cf2
FabGiese <fabgiese@gmail.com>
2023-02-20 11:40:40 -0300

    tratamento de erro 400 de validacao

--- src/main/java/med/voll/api/infra/TratadorDeErros.java
+++ src/main/java/med/voll/api/infra/TratadorDeErros.java
@@ -2,6 +2,8 @@ package med.voll.api.infra;
 
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -12,4 +14,17 @@ public class TratadorDeErros {
     public ResponseEntity tratarEntidadeNotFound() {
         return ResponseEntity.notFound().build();
     }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity tratarValidacaoDados(MethodArgumentNotValidException ex){
+        var erros = ex.getFieldErrors();
+        return ResponseEntity.badRequest().body(erros.stream().map(DadosValidacao::new).toList());
+    }
+
+    private record DadosValidacao(String campo, String mensagem){
+        private DadosValidacao(FieldError erro){
+            this(erro.getField(), erro.getDefaultMessage());
+        }
+    }
+
 }
