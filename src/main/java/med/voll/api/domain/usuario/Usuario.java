f44385ecc217b9f3ec2e7ee5b5261df417db3af1
FabGiese <fabgiese@gmail.com>
2023-02-21 12:58:58 -0300

    adicionado entidade Usuario e cricao da table usuarios no banco pelo migration.

---
+++ src/main/java/med/voll/api/domain/usuario/Usuario.java
@@ -0,0 +1,19 @@
+package med.voll.api.domain.usuario;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity(name = "Usuario")
+@Table(name = "usuarios")
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String login;
+    private String senha;
+}
