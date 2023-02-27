7e30a6d5f236ac9d28b3882145b71af671007d1e
FabGiese <fabgiese@gmail.com>
2023-02-26 21:51:03 -0300

    adicionado tratamento de integridade medico e especialidade e adicionado query para retornar um medico aleatorio que estaja livre na data

--- src/main/java/med/voll/api/domain/consulta/AgendaDeConsulta.java
+++ src/main/java/med/voll/api/domain/consulta/AgendaDeConsulta.java
@@ -1,5 +1,6 @@
 package med.voll.api.domain.consulta;
 
+import jakarta.validation.ValidationException;
 import med.voll.api.domain.ValidacaoException;
 import med.voll.api.domain.medico.Medico;
 import med.voll.api.domain.medico.MedicoRepository;
@@ -19,20 +20,28 @@ public class AgendaDeConsulta {
     @Autowired
     private PacienteRepository pacienteRepository;
 
-    public void agendar(DadosAgendamentoConsulta dados) throws ValidacaoException {
+    public void agendar(DadosAgendamentoConsulta dados) {
         if(!pacienteRepository.existsById(dados.idPaciente())) {
             throw new ValidacaoException("Id do paciente não existe.");
         }
         if(dados.idMedico() != null && medicoRepository.existsById(dados.idMedico())) {
             throw new ValidacaoException("Id do médico não existe.");
         }
-        var paciente = pacienteRepository.findById(dados.idPaciente()).get();
+        var paciente = pacienteRepository.getReferenceById(dados.idPaciente());
         var medico = escolherMedico(dados);
         var consulta = new Consulta(null, medico, paciente, dados.data());
         consultaRepository.save(consulta);
     }
 
     private Medico escolherMedico(DadosAgendamentoConsulta dados) {
-        return null;
+        if(dados.idMedico() != null) {
+            return medicoRepository.getReferenceById(dados.idMedico());
+        }
+
+        if(dados.especialidade() == null) {
+            throw new ValidationException("Precisa escolher uma especialidade se não existir médico de preferencia!");
+        }
+
+        return medicoRepository.getMedicoAleatorioLivreNaData(dados.especialidade(), dados.data());
     }
 }
